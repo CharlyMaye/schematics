214a3c150cb4a42a0e4a21fe026e721362b73d75
Charly Mayeux <34808521+CharlyMaye@users.noreply.github.com>
2023-09-02 14:47:44 +0200

    Adding basic master mechanics

--- schematics/src/tests/index.spec.ts
+++ schematics/src/tests/index.spec.ts
@@ -1,12 +1,18 @@
 import { join } from 'node:path';
 import { engine } from './utils/engine';
+import { createFiles, readFiles } from './utils/master';
 
 const collectionPath = join(__dirname, '../collection.json');
 
 describe('schematics', () => {
     it('works', async () => {
-        const base = await import('./files/test/work_1/base.json');
+        const testPath = join(__dirname, './files/test/work_1');
+        const base = await import(join(testPath, 'base.json'));
         const tree = await engine('schematics', collectionPath)(base);
-        expect(tree.files.length).toEqual(21);
+
+        createFiles(tree, testPath, 'result');
+        const masters = readFiles(testPath, 'master');
+        const result = readFiles(testPath, 'result');
+        expect(masters.length).toEqual(result.length);
     });
 });
---
+++ schematics/src/tests/utils/master.ts
@@ -0,0 +1,57 @@
+import { join, parse } from 'node:path';
+import {
+    existsSync,
+    mkdirSync,
+    readdirSync,
+    readFileSync,
+    statSync,
+    writeFileSync,
+} from 'node:fs';
+import { Tree } from '@angular-devkit/schematics';
+
+export function createFiles(
+    tree: Tree,
+    testPath: string,
+    outputType: 'master' | 'result' = 'result'
+) {
+    //  Génération de fichier
+    tree.visit((path, entry) => {
+        const pathToGenerate = join(testPath, outputType, path);
+        try {
+            if (!existsSync(parse(pathToGenerate).dir)) {
+                mkdirSync(parse(pathToGenerate).dir, { recursive: true });
+            }
+            writeFileSync(pathToGenerate, entry!.content.toString(), {
+                encoding: 'utf8',
+            });
+        } catch (error) {
+            console.error(error);
+        }
+    });
+}
+
+// Recursive function to get files
+function getFiles(dir: string, files: string[] = []) {
+    const fileList = readdirSync(dir, { encoding: 'utf8' });
+    for (const file of fileList) {
+        const name = `${dir}/${file}`;
+        if (statSync(name).isDirectory()) {
+            getFiles(name, files);
+        } else {
+            files.push(name);
+        }
+    }
+    return files;
+}
+
+export function readFiles(
+    testPath: string,
+    inputType: 'master' | 'result' = 'result'
+) {
+    const pathToRead = join(testPath, inputType);
+    const files = getFiles(pathToRead);
+    return files.map((file) => ({
+        path: file,
+        content: readFileSync(file, { encoding: 'utf8' }),
+    }));
+}
